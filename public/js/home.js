93ac92d36dd6504d6b13036c4830e279abdcf6ed
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-01-31 16:17:22 +0100

    :sparkles: Add course management functionality: implement course addition form, enhance homepage layout, and update page elements for better user experience

--- public/js/home.js
+++ public/js/home.js
@@ -7,6 +7,11 @@ let navbarButton;
 let profileName;
 let profileRole;
 let homeTitle;
+let homeDescription;
+let addCourseForm;
+let addCourseInputs;
+let addCourseTextArea;
+let cardsContainer;
 let logoutButton = document.querySelector("#logout-button");
 let navbarActive = false;
 // Function to initialize the home page. This function will be called when the home page is loaded in app.ts.
@@ -17,6 +22,11 @@ export const initializeHome = () => {
     profileName = navbar.querySelector(".profile-name");
     profileRole = navbar.querySelector(".profile-role");
     homeTitle = document.querySelector(".home-title");
+    homeDescription = document.querySelector(".home-description");
+    cardsContainer = document.querySelector(".cards-container");
+    addCourseForm = document.querySelector(".add-course-form");
+    addCourseInputs = addCourseForm.querySelectorAll("input");
+    addCourseTextArea = addCourseForm.querySelector("textarea");
     navbarButton === null || navbarButton === void 0 ? void 0 : navbarButton.addEventListener("click", () => {
         toggleNavbar();
     });
@@ -39,6 +49,7 @@ export const initializeHome = () => {
     if (location.hash === "") {
         initializeCourses();
     }
+    hashChange();
     window.addEventListener("hashchange", hashChange);
 };
 // Function that uupdates all the text in the site to the authenticated user's name and role. This will always be authenticated since the user has to be authenticated to access the home page.
@@ -68,4 +79,27 @@ const hashChange = () => {
     if (location.hash === "") {
         initializeCourses();
     }
+    if (location.hash === "#addCourse") {
+        updatePageElement(homeTitle, "Add Course");
+        updatePageElement(homeDescription, "Fill in the details of the course below and click the 'Add Course' button to add the course to the list of courses.");
+        cardsContainer.classList.add("hidden");
+        document.querySelector(".add-course-container").classList.remove("hidden");
+        document.querySelector(".main-content-title").classList.add("hidden");
+        addCourseForm.querySelectorAll("input").forEach((input) => {
+            input.addEventListener("input", () => {
+                updatePreviewCard(input);
+            });
+        });
+        addCourseForm.querySelector("textarea").addEventListener("input", () => {
+            updatePreviewCard(addCourseForm.querySelector("textarea"));
+        });
+    }
+};
+// Function that updates the text in the page element. Why I added this function is so the fade-in effect will also be applied to the text in the page element.
+const updatePageElement = (element, text) => {
+    element.textContent = text;
+    element.classList.add("fade-in");
+};
+const updatePreviewCard = (element) => {
+    document.querySelector(".preview-container").innerHTML = element.value;
 };
--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -10,6 +10,13 @@ let navbarButton: HTMLElement;
 let profileName: HTMLElement;
 let profileRole: HTMLElement;
 let homeTitle: HTMLElement;
+let homeDescription: HTMLElement;
+let addCourseForm: HTMLElement;
+let addCourseInputs: NodeListOf<HTMLInputElement>;
+
+let addCourseTextArea: HTMLElement;
+
+let cardsContainer: HTMLElement;
 
 let logoutButton = document.querySelector(
   "#logout-button"
@@ -25,6 +32,15 @@ export const initializeHome = () => {
   profileName = navbar.querySelector(".profile-name") as HTMLElement;
   profileRole = navbar.querySelector(".profile-role") as HTMLElement;
   homeTitle = document.querySelector(".home-title") as HTMLElement;
+  homeDescription = document.querySelector(".home-description") as HTMLElement;
+  cardsContainer = document.querySelector(".cards-container") as HTMLElement;
+  addCourseForm = document.querySelector(".add-course-form") as HTMLFormElement;
+  addCourseInputs = addCourseForm.querySelectorAll(
+    "input"
+  ) as NodeListOf<HTMLInputElement>;
+  addCourseTextArea = addCourseForm.querySelector(
+    "textarea"
+  ) as HTMLTextAreaElement;
 
   navbarButton?.addEventListener("click", () => {
     toggleNavbar();
@@ -56,6 +72,7 @@ export const initializeHome = () => {
     initializeCourses();
   }
 
+  hashChange();
   window.addEventListener("hashchange", hashChange);
 };
 
@@ -92,4 +109,35 @@ const hashChange = () => {
   if (location.hash === "") {
     initializeCourses();
   }
+
+  if (location.hash === "#addCourse") {
+    updatePageElement(homeTitle, "Add Course");
+    updatePageElement(
+      homeDescription,
+      "Fill in the details of the course below and click the 'Add Course' button to add the course to the list of courses."
+    );
+
+    cardsContainer.classList.add("hidden");
+    document.querySelector(".add-course-container")!.classList.remove("hidden");
+    document.querySelector(".main-content-title")!.classList.add("hidden");
+
+    addCourseForm.querySelectorAll("input").forEach((input) => {
+      input.addEventListener("input", () => {
+        updatePreviewCard(input);
+      });
+    });
+    addCourseForm.querySelector("textarea")!.addEventListener("input", () => {
+      updatePreviewCard(addCourseForm.querySelector("textarea")!);
+    });
+  }
+};
+
+// Function that updates the text in the page element. Why I added this function is so the fade-in effect will also be applied to the text in the page element.
+const updatePageElement = (element: HTMLElement, text: string) => {
+  element.textContent = text;
+  element.classList.add("fade-in");
+};
+
+const updatePreviewCard = (element: any) => {
+  document.querySelector(".preview-container")!.innerHTML = element.value;
 };
