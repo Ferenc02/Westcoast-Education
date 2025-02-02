880ba2b04cbf4a096ae4a0ecd8ee6e24bc8d144d
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-02 19:10:54 +0100

    :recycle: Improve course display elements: modify user data structure, enhance checkbox IDs, and refine course preview

--- public/js/home.js
+++ public/js/home.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { signOutUser } from "./authentication.js";
 import { authenticatedUser } from "./app.js";
+import showMessageBox from "./errorHandling.js";
 import { initializeCourses } from "./courses.js";
 let header;
 let navbar;
@@ -18,10 +19,24 @@ let profileRole;
 let homeTitle;
 let homeDescription;
 let addCourseForm;
-let addCourseInputs;
-let addCourseTextArea;
+let addCourseFormName;
+let addCourseFormDescription;
+let addCourseFormImage;
+let addCourseFormLocation1;
+let addCourseFormLocation2;
+let addCourseFormInstructor;
+let addCourseFormPrice;
+let addCourseFormStartDate;
+let addCourseFormEndDate;
 let cardsContainer;
 let previewContainer;
+let coursePreviewImage;
+let coursePreviewName;
+let coursePreviewDescription;
+let coursePreviewInstructor;
+let coursePreviewPrice;
+let coursePreviewDate;
+let coursePreviewLocation;
 let logoutButton = document.querySelector("#logout-button");
 let navbarActive = false;
 // Function to initialize the home page. This function will be called when the home page is loaded in app.ts.
@@ -35,9 +50,6 @@ export const initializeHome = () => {
     homeDescription = document.querySelector(".home-description");
     cardsContainer = document.querySelector(".cards-container");
     addCourseForm = document.querySelector(".add-course-form");
-    addCourseInputs = addCourseForm.querySelectorAll("input");
-    addCourseTextArea = addCourseForm.querySelector("textarea");
-    previewContainer = document.querySelector(".preview-container");
     navbarButton === null || navbarButton === void 0 ? void 0 : navbarButton.addEventListener("click", () => {
         toggleNavbar();
     });
@@ -91,20 +103,58 @@ const hashChange = () => {
         initializeCourses();
     }
     if (location.hash === "#addCourse") {
-        updatePageElement(homeTitle, "Add Course");
-        updatePageElement(homeDescription, "Fill in the details of the course below and click the 'Add Course' button to add the course to the list of courses.");
+        // Add course form elements *left side*
+        addCourseFormName = addCourseForm.querySelector("#course-name-input");
+        addCourseFormDescription = addCourseForm.querySelector("#course-description-input");
+        addCourseFormImage = addCourseForm.querySelector("#course-image-input");
+        addCourseFormInstructor = addCourseForm.querySelector("#course-instructor-input");
+        addCourseFormPrice = addCourseForm.querySelector("#course-price-input");
+        addCourseFormStartDate = addCourseForm.querySelector("#course-start-date-input");
+        addCourseFormEndDate = addCourseForm.querySelector("#course-end-date-input");
+        addCourseFormLocation1 = addCourseForm.querySelector("#course-checkbox-1");
+        addCourseFormLocation2 = addCourseForm.querySelector("#course-checkbox-2");
+        addCourseFormName.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormName, coursePreviewName);
+        });
+        addCourseFormDescription.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormDescription, coursePreviewDescription);
+        });
+        addCourseFormImage.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormImage, coursePreviewImage);
+        });
+        addCourseFormInstructor.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormInstructor, coursePreviewInstructor);
+        });
+        addCourseFormPrice.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormPrice, coursePreviewPrice);
+        });
+        addCourseFormStartDate.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormStartDate, coursePreviewDate);
+        });
+        addCourseFormEndDate.addEventListener("input", () => {
+            updatePreviewCard(addCourseFormEndDate, coursePreviewDate);
+        });
+        addCourseFormLocation1.addEventListener("change", () => {
+            updatePreviewCard(addCourseFormLocation1, coursePreviewLocation);
+        });
+        addCourseFormLocation2.addEventListener("change", () => {
+            updatePreviewCard(addCourseFormLocation2, coursePreviewLocation);
+        });
+        //  Preview card elements
+        previewContainer = document.querySelector(".preview-container");
+        coursePreviewImage = previewContainer.querySelector(".course-preview-image");
+        coursePreviewName = previewContainer.querySelector(".course-preview-name");
+        coursePreviewDescription = previewContainer.querySelector(".course-preview-description");
+        coursePreviewInstructor = previewContainer.querySelector(".course-preview-instructor");
+        coursePreviewPrice = previewContainer.querySelector(".course-preview-price");
+        coursePreviewDate = previewContainer.querySelector(".course-preview-date");
+        coursePreviewLocation = previewContainer.querySelector(".course-preview-location");
+        setTextContent(homeTitle, "Add Course");
+        setTextContent(homeDescription, "Fill in the details of the course below and click the 'Add Course' button to add the course to the list of courses.");
         cardsContainer.classList.add("hidden");
         document.querySelector(".add-course-container").classList.remove("hidden");
         document.querySelector(".main-content-title").classList.add("hidden");
         setRandomImage(); // Set a random image when the page is loaded.
-        addCourseForm.querySelectorAll("input").forEach((input) => {
-            input.addEventListener("input", () => {
-                updatePreviewCard();
-            });
-        });
-        addCourseForm.querySelector("textarea").addEventListener("input", () => {
-            updatePreviewCard();
-        });
         const imageCheckbox = addCourseForm.querySelector("#image-checkbox");
         const courseImageInput = document.querySelector("#course-image-input");
         imageCheckbox === null || imageCheckbox === void 0 ? void 0 : imageCheckbox.addEventListener("change", () => {
@@ -145,44 +195,89 @@ const setRandomImage = () => {
     });
 };
 // Function that updates the text in the page element. Why I added this function is so the fade-in effect will also be applied to the text in the page element.
-const updatePageElement = (element, text) => {
+const setTextContent = (element, text) => {
     element.textContent = text;
     element.classList.add("fade-in");
 };
-const updatePreviewCard = () => {
-    var _a, _b, _c, _d, _e, _f;
-    // Course name
-    previewContainer.querySelector(".course-name").textContent = (_a = addCourseForm.querySelector("#course-name-input")) === null || _a === void 0 ? void 0 : _a.value;
-    // Description
-    previewContainer.querySelector(".course-description").textContent = (_b = addCourseForm.querySelector("#course-description-input")) === null || _b === void 0 ? void 0 : _b.value;
-    // Checkboxes
-    const checkboxes = [
-        { id: "#location-checkbox-1", label: "Campus" },
-        { id: "#location-checkbox-2", label: "Online" },
-    ];
-    const selectedLocations = checkboxes
-        .filter(({ id }) => addCourseForm.querySelector(id).checked)
-        .map(({ label }) => label)
-        .join(" & ");
-    previewContainer.querySelector("#course-location").textContent =
-        selectedLocations;
-    // Image
-    const imageInput = addCourseForm.querySelector("#course-image-input");
-    const image = imageInput.value;
-    try {
-        new URL(image);
-        previewContainer.querySelector("#course-image").setAttribute("src", image);
+const updatePreviewCard = (element, previewElement) => {
+    const { id, value } = element;
+    if (id === "course-price-input") {
+        setTextContent(previewElement, `$${value}`);
+        return;
     }
-    catch (error) { }
-    // Instructor
-    previewContainer.querySelector("#course-instructor").textContent = (_c = addCourseForm.querySelector("#course-instructor-input")) === null || _c === void 0 ? void 0 : _c.value;
-    // Price
-    previewContainer.querySelector("#course-price").textContent =
-        "$" +
-            ((_d = addCourseForm.querySelector("#course-price-input")) === null || _d === void 0 ? void 0 : _d.value);
-    // Date
-    const startDate = (_e = addCourseForm.querySelector("#course-start-date-input")) === null || _e === void 0 ? void 0 : _e.value;
-    const endDate = (_f = addCourseForm.querySelector("#course-end-date-input")) === null || _f === void 0 ? void 0 : _f.value;
-    previewContainer.querySelector("#course-date").textContent =
-        startDate + " - " + endDate;
+    if (id === "course-start-date-input" || id === "course-end-date-input") {
+        const startDate = addCourseFormStartDate.value || "";
+        const endDate = addCourseFormEndDate.value || "";
+        const difference = Date.parse(endDate) - Date.parse(startDate);
+        if (difference < 0) {
+            showMessageBox("The start date must be before the end date.", "error");
+            element.value = "";
+            return;
+        }
+        setTextContent(previewElement, `${startDate} - ${endDate}`);
+        return;
+    }
+    if (id === "course-checkbox-1" || id === "course-checkbox-2") {
+        const selectedLocations = [addCourseFormLocation1, addCourseFormLocation2]
+            .filter((checkbox) => checkbox.checked)
+            .map((checkbox) => {
+            const label = document.querySelector(`label[for="${checkbox.id}"]`);
+            return label ? label.textContent : checkbox.value;
+        })
+            .join(" & ");
+        setTextContent(previewElement, selectedLocations);
+        return;
+    }
+    if (id === "course-image-input") {
+        try {
+            new URL(element.value);
+            previewElement.setAttribute("src", element.value);
+        }
+        catch (error) {
+            previewElement.setAttribute("src", "");
+        }
+        return;
+    }
+    // previewElement.textContent = element.value;
+    setTextContent(previewElement, element.value);
+    // // Checkboxes
+    // const checkboxes = [
+    //   { id: "#location-checkbox-1", label: "Campus" },
+    //   { id: "#location-checkbox-2", label: "Online" },
+    // ];
+    // const selectedLocations = checkboxes
+    //   .filter(
+    //     ({ id }) => (addCourseForm.querySelector(id) as HTMLInputElement).checked
+    //   )
+    //   .map(({ label }) => label)
+    //   .join(" & ");
+    // previewContainer.querySelector("#course-location")!.textContent =
+    //   selectedLocations;
+    // // Image
+    // const imageInput = addCourseForm.querySelector(
+    //   "#course-image-input"
+    // ) as HTMLInputElement;
+    // const image = imageInput.value;
+    // try {
+    //   new URL(image);
+    //   previewContainer.querySelector("#course-image")!.setAttribute("src", image);
+    // } catch (error) {}
+    // // Instructor
+    // previewContainer.querySelector("#course-instructor")!.textContent = (
+    //   addCourseForm.querySelector("#course-instructor-input") as HTMLInputElement
+    // )?.value;
+    // // Price
+    // previewContainer.querySelector("#course-price")!.textContent =
+    //   "$" +
+    //   (addCourseForm.querySelector("#course-price-input") as HTMLInputElement)
+    //     ?.value;
+    // // Date
+    // const startDate = (
+    //   addCourseForm.querySelector("#course-start-date-input") as HTMLInputElement
+    // )?.value;
+    // const endDate = (
+    //   addCourseForm.querySelector("#course-end-date-input") as HTMLInputElement
+    // )?.value;
+    // previewContainer.querySelector("#course-date")!.textContent =
+    //   startDate + " - " + endDate;
 };
--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -12,10 +12,27 @@ let profileRole: HTMLElement;
 let homeTitle: HTMLElement;
 let homeDescription: HTMLElement;
 let addCourseForm: HTMLElement;
-let addCourseInputs: NodeListOf<HTMLInputElement>;
-let addCourseTextArea: HTMLElement;
+
+let addCourseFormName: HTMLInputElement;
+let addCourseFormDescription: HTMLTextAreaElement;
+let addCourseFormImage: HTMLInputElement;
+let addCourseFormLocation1: HTMLInputElement;
+let addCourseFormLocation2: HTMLInputElement;
+let addCourseFormInstructor: HTMLInputElement;
+let addCourseFormPrice: HTMLInputElement;
+let addCourseFormStartDate: HTMLInputElement;
+let addCourseFormEndDate: HTMLInputElement;
+
 let cardsContainer: HTMLElement;
 let previewContainer: HTMLElement;
+
+let coursePreviewImage: HTMLImageElement;
+let coursePreviewName: HTMLElement;
+let coursePreviewDescription: HTMLElement;
+let coursePreviewInstructor: HTMLElement;
+let coursePreviewPrice: HTMLElement;
+let coursePreviewDate: HTMLElement;
+let coursePreviewLocation: HTMLElement;
 
 let logoutButton = document.querySelector(
   "#logout-button"
@@ -34,16 +51,6 @@ export const initializeHome = () => {
   homeDescription = document.querySelector(".home-description") as HTMLElement;
   cardsContainer = document.querySelector(".cards-container") as HTMLElement;
   addCourseForm = document.querySelector(".add-course-form") as HTMLFormElement;
-  addCourseInputs = addCourseForm.querySelectorAll(
-    "input"
-  ) as NodeListOf<HTMLInputElement>;
-  addCourseTextArea = addCourseForm.querySelector(
-    "textarea"
-  ) as HTMLTextAreaElement;
-
-  previewContainer = document.querySelector(
-    ".preview-container"
-  ) as HTMLElement;
 
   navbarButton?.addEventListener("click", () => {
     toggleNavbar();
@@ -114,8 +121,115 @@ const hashChange = () => {
   }
 
   if (location.hash === "#addCourse") {
-    updatePageElement(homeTitle, "Add Course");
-    updatePageElement(
+    // Add course form elements *left side*
+
+    addCourseFormName = addCourseForm.querySelector(
+      "#course-name-input"
+    ) as HTMLInputElement;
+
+    addCourseFormDescription = addCourseForm.querySelector(
+      "#course-description-input"
+    ) as HTMLTextAreaElement;
+
+    addCourseFormImage = addCourseForm.querySelector(
+      "#course-image-input"
+    ) as HTMLInputElement;
+
+    addCourseFormInstructor = addCourseForm.querySelector(
+      "#course-instructor-input"
+    ) as HTMLInputElement;
+
+    addCourseFormPrice = addCourseForm.querySelector(
+      "#course-price-input"
+    ) as HTMLInputElement;
+
+    addCourseFormStartDate = addCourseForm.querySelector(
+      "#course-start-date-input"
+    ) as HTMLInputElement;
+
+    addCourseFormEndDate = addCourseForm.querySelector(
+      "#course-end-date-input"
+    ) as HTMLInputElement;
+
+    addCourseFormLocation1 = addCourseForm.querySelector(
+      "#course-checkbox-1"
+    ) as HTMLInputElement;
+
+    addCourseFormLocation2 = addCourseForm.querySelector(
+      "#course-checkbox-2"
+    ) as HTMLInputElement;
+
+    addCourseFormName.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormName, coursePreviewName);
+    });
+
+    addCourseFormDescription.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormDescription, coursePreviewDescription);
+    });
+
+    addCourseFormImage.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormImage, coursePreviewImage);
+    });
+
+    addCourseFormInstructor.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormInstructor, coursePreviewInstructor);
+    });
+
+    addCourseFormPrice.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormPrice, coursePreviewPrice);
+    });
+
+    addCourseFormStartDate.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormStartDate, coursePreviewDate);
+    });
+
+    addCourseFormEndDate.addEventListener("input", () => {
+      updatePreviewCard(addCourseFormEndDate, coursePreviewDate);
+    });
+
+    addCourseFormLocation1.addEventListener("change", () => {
+      updatePreviewCard(addCourseFormLocation1, coursePreviewLocation);
+    });
+
+    addCourseFormLocation2.addEventListener("change", () => {
+      updatePreviewCard(addCourseFormLocation2, coursePreviewLocation);
+    });
+
+    //  Preview card elements
+    previewContainer = document.querySelector(
+      ".preview-container"
+    ) as HTMLElement;
+
+    coursePreviewImage = previewContainer.querySelector(
+      ".course-preview-image"
+    ) as HTMLImageElement;
+
+    coursePreviewName = previewContainer.querySelector(
+      ".course-preview-name"
+    ) as HTMLElement;
+
+    coursePreviewDescription = previewContainer.querySelector(
+      ".course-preview-description"
+    ) as HTMLElement;
+
+    coursePreviewInstructor = previewContainer.querySelector(
+      ".course-preview-instructor"
+    ) as HTMLElement;
+
+    coursePreviewPrice = previewContainer.querySelector(
+      ".course-preview-price"
+    ) as HTMLElement;
+
+    coursePreviewDate = previewContainer.querySelector(
+      ".course-preview-date"
+    ) as HTMLElement;
+
+    coursePreviewLocation = previewContainer.querySelector(
+      ".course-preview-location"
+    ) as HTMLElement;
+
+    setTextContent(homeTitle, "Add Course");
+    setTextContent(
       homeDescription,
       "Fill in the details of the course below and click the 'Add Course' button to add the course to the list of courses."
     );
@@ -125,15 +239,6 @@ const hashChange = () => {
     document.querySelector(".main-content-title")!.classList.add("hidden");
 
     setRandomImage(); // Set a random image when the page is loaded.
-
-    addCourseForm.querySelectorAll("input").forEach((input) => {
-      input.addEventListener("input", () => {
-        updatePreviewCard();
-      });
-    });
-    addCourseForm.querySelector("textarea")!.addEventListener("input", () => {
-      updatePreviewCard();
-    });
 
     const imageCheckbox = addCourseForm.querySelector(
       "#image-checkbox"
@@ -188,73 +293,115 @@ const setRandomImage = () => {
 };
 
 // Function that updates the text in the page element. Why I added this function is so the fade-in effect will also be applied to the text in the page element.
-const updatePageElement = (element: HTMLElement, text: string) => {
+const setTextContent = (element: HTMLElement, text: string) => {
   element.textContent = text;
   element.classList.add("fade-in");
 };
 
-const updatePreviewCard = () => {
-  // Course name
-  previewContainer.querySelector(".course-name")!.textContent = (
-    addCourseForm.querySelector("#course-name-input") as HTMLInputElement
-  )?.value;
+const updatePreviewCard = (
+  element: HTMLInputElement | HTMLTextAreaElement,
+  previewElement: HTMLElement
+) => {
+  const { id, value } = element;
 
-  // Description
-  previewContainer.querySelector(".course-description")!.textContent = (
-    addCourseForm.querySelector(
-      "#course-description-input"
-    ) as HTMLTextAreaElement
-  )?.value;
+  if (id === "course-price-input") {
+    setTextContent(previewElement, `$${value}`);
+    return;
+  }
+  if (id === "course-start-date-input" || id === "course-end-date-input") {
+    const startDate = addCourseFormStartDate.value || "";
+    const endDate = addCourseFormEndDate.value || "";
 
-  // Checkboxes
-  const checkboxes = [
-    { id: "#location-checkbox-1", label: "Campus" },
-    { id: "#location-checkbox-2", label: "Online" },
-  ];
+    const difference = Date.parse(endDate) - Date.parse(startDate);
 
-  const selectedLocations = checkboxes
-    .filter(
-      ({ id }) => (addCourseForm.querySelector(id) as HTMLInputElement).checked
-    )
-    .map(({ label }) => label)
-    .join(" & ");
+    if (difference < 0) {
+      showMessageBox("The start date must be before the end date.", "error");
+      element.value = "";
+      return;
+    }
 
-  previewContainer.querySelector("#course-location")!.textContent =
-    selectedLocations;
+    setTextContent(previewElement, `${startDate} - ${endDate}`);
 
-  // Image
-  const imageInput = addCourseForm.querySelector(
-    "#course-image-input"
-  ) as HTMLInputElement;
+    return;
+  }
 
-  const image = imageInput.value;
+  if (id === "course-checkbox-1" || id === "course-checkbox-2") {
+    const selectedLocations = [addCourseFormLocation1, addCourseFormLocation2]
+      .filter((checkbox) => checkbox.checked)
+      .map((checkbox) => {
+        const label = document.querySelector(`label[for="${checkbox.id}"]`);
+        return label ? label.textContent : checkbox.value;
+      })
+      .join(" & ");
 
-  try {
-    new URL(image);
-    previewContainer.querySelector("#course-image")!.setAttribute("src", image);
-  } catch (error) {}
+    setTextContent(previewElement, selectedLocations);
+    return;
+  }
 
-  // Instructor
-  previewContainer.querySelector("#course-instructor")!.textContent = (
-    addCourseForm.querySelector("#course-instructor-input") as HTMLInputElement
-  )?.value;
+  if (id === "course-image-input") {
+    try {
+      new URL(element.value);
+      previewElement.setAttribute("src", element.value);
+    } catch (error) {
+      previewElement.setAttribute("src", "");
+    }
 
-  // Price
-  previewContainer.querySelector("#course-price")!.textContent =
-    "$" +
-    (addCourseForm.querySelector("#course-price-input") as HTMLInputElement)
-      ?.value;
+    return;
+  }
 
-  // Date
+  // previewElement.textContent = element.value;
 
-  const startDate = (
-    addCourseForm.querySelector("#course-start-date-input") as HTMLInputElement
-  )?.value;
+  setTextContent(previewElement, element.value);
 
-  const endDate = (
-    addCourseForm.querySelector("#course-end-date-input") as HTMLInputElement
-  )?.value;
+  // // Checkboxes
+  // const checkboxes = [
+  //   { id: "#location-checkbox-1", label: "Campus" },
+  //   { id: "#location-checkbox-2", label: "Online" },
+  // ];
 
-  previewContainer.querySelector("#course-date")!.textContent =
-    startDate + " - " + endDate;
+  // const selectedLocations = checkboxes
+  //   .filter(
+  //     ({ id }) => (addCourseForm.querySelector(id) as HTMLInputElement).checked
+  //   )
+  //   .map(({ label }) => label)
+  //   .join(" & ");
+
+  // previewContainer.querySelector("#course-location")!.textContent =
+  //   selectedLocations;
+
+  // // Image
+  // const imageInput = addCourseForm.querySelector(
+  //   "#course-image-input"
+  // ) as HTMLInputElement;
+
+  // const image = imageInput.value;
+
+  // try {
+  //   new URL(image);
+  //   previewContainer.querySelector("#course-image")!.setAttribute("src", image);
+  // } catch (error) {}
+
+  // // Instructor
+  // previewContainer.querySelector("#course-instructor")!.textContent = (
+  //   addCourseForm.querySelector("#course-instructor-input") as HTMLInputElement
+  // )?.value;
+
+  // // Price
+  // previewContainer.querySelector("#course-price")!.textContent =
+  //   "$" +
+  //   (addCourseForm.querySelector("#course-price-input") as HTMLInputElement)
+  //     ?.value;
+
+  // // Date
+
+  // const startDate = (
+  //   addCourseForm.querySelector("#course-start-date-input") as HTMLInputElement
+  // )?.value;
+
+  // const endDate = (
+  //   addCourseForm.querySelector("#course-end-date-input") as HTMLInputElement
+  // )?.value;
+
+  // previewContainer.querySelector("#course-date")!.textContent =
+  //   startDate + " - " + endDate;
 };
